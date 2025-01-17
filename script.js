8d9e371acf2f89f746445632a3f767f71e3e9d50
Mikael Johnsson <mikaeljohanjohnsson@gmail.com>
2025-01-17 11:18:57 +0100

    add function to continue to next page in form modal

---
+++ script.js
@@ -0,0 +1,21 @@
+
+// Modal
+const formModal = document.getElementById('formModal');
+const pageOne = formModal.querySelector('#pageOne');
+const pageTwo = formModal.querySelector('#pageTwo');
+const submitButton = formModal.querySelector('#submitButton');
+
+submitButton.addEventListener('click', function(event){
+    event.preventDefault();
+    let pages = Array.from(formModal.querySelectorAll('.page'));
+    let visiblePages = pages.filter((page) => !page.classList.contains('d-none'));
+    let nextPage = visiblePages[0].nextElementSibling;
+    
+    if(nextPage == null){
+        console.log("this was the last page, time to submit form");
+    } else {
+        visiblePages[0].classList.add('d-none');
+        nextPage.classList.remove('d-none');
+    }
+    
+});
