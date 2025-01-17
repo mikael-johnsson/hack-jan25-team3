721829d9f035b5f6dd1f76039a92c68d9c4d354b
Mikael Johnsson <mikaeljohanjohnsson@gmail.com>
2025-01-17 14:26:35 +0100

    add seeReports, configure submitForm to working towards backend server

--- script.js
+++ script.js
@@ -1,14 +1,18 @@
 
-// Modal
+// Modal elements
 const formModal = document.getElementById('formModal'); //Entire Modal
 const modalButton = formModal.querySelector('#submitButton'); // Continue /submit button
 const descriptionInput = document.getElementById('incidentDescription'); // Description input element
 const mediaUploads = document.getElementById('mediaUpload'); // Media upload input element
+
+// Form Variables to save data temporarily
 let incidentDescription= "p-holder";
 let uploadedMedia = [];
 let uploadedMediaNames = [];
+
+// Database URLs
 const databaseURLTest = "https://haven-v1-fafcc90518dc.herokuapp.com/api/test"; // URL to the database
-const databaseURL = "https://haven-v1-fafcc90518dc.herokuapp.com"; // URL to the database
+const databaseURL = "https://haven-v1-fafcc90518dc.herokuapp.com/api"; // URL to the database
 
 /**
  * When clicking Continue/Submit button, the form will move to the next page
@@ -29,7 +33,7 @@ function changePage(){
     let pages = Array.from(formModal.querySelectorAll('.page'));
     let visiblePages = pages.filter((page) => !page.classList.contains('d-none'));
     let nextPage = visiblePages[0].nextElementSibling;
-    console.log(nextPage);
+    
     if(!nextPage){
         submitForm();
         console.log("this was the last page, time to submit form");
@@ -53,26 +57,41 @@ function saveFormInputs(){
     uploadedMediaNames = uploadedMedia.map((file) => file.name); // Save the uploaded media names
 }
 
+
+/**
+ * Submit the form to the database
+ */
 async function submitForm(){
-    try{
+    try {
         const formData = new FormData();
         formData.append('description', incidentDescription);
         uploadedMedia.forEach((file) => formData.append('media', file));
-        const response = await fetch(databaseURLTest);
-        const data = await response.json();
-        console.log(data);
-
         for (let pair of formData.entries()) {
-            console.log("all formdata entries", pair[0] + ': ' + pair[1]);
+            console.log("formdata entry", pair[0] + ': ' + pair[1]);
         }
+        const response = await fetch(`${databaseURL}/report`, {
+            method: 'POST',
+            body: formData
+        });
+        const data = await response.json();
+        console.log("response data: ", data);
     }
     catch(e){
-        console.log(e)
+        console.log("error: ", e)
     }
 }
 
 
-// , {
-//     method: 'POST',
-//     body: formData
-// }
+/**
+ * See all reports from api
+ */
+async function seeReports(){
+    try{
+        const data = await fetch(`${databaseURL}/all-results`, {
+            method: 'GET'
+        });
+        console.log("data: ", data);
+    }catch(e){
+        console.log("error: ", e)
+    }
+}
