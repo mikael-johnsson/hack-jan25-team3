64160248fc780284affc1e563e097a16d9578b34
Mikael Johnsson <mikaeljohanjohnsson@gmail.com>
2025-01-17 11:22:56 +0100

    make continue button change to submit when on last page

--- script.js
+++ script.js
@@ -10,10 +10,13 @@ submitButton.addEventListener('click', function(event){
     let pages = Array.from(formModal.querySelectorAll('.page'));
     let visiblePages = pages.filter((page) => !page.classList.contains('d-none'));
     let nextPage = visiblePages[0].nextElementSibling;
-    
+
     if(nextPage == null){
         console.log("this was the last page, time to submit form");
     } else {
+        if(nextPage.id === "pageThree"){
+            submitButton.textContent = "Submit";
+        }
         visiblePages[0].classList.add('d-none');
         nextPage.classList.remove('d-none');
     }
