1e6a7cc3a55553f656299f5f6ae71fd4150156fa
Gareth Whitley <garethwhitleychard@gmail.com>
2025-01-19 23:09:07 +0000

    style: change floating quick escape to a button tag for better clickability and add aria labels

--- assets/js/escape.js
+++ assets/js/escape.js
@@ -1,7 +1,7 @@
 // Logic for escape button in the nav bar: redirects the user to google and replaces the last page in
 // the history with '/news' so as not to appear suspicious
-const escapeButtonMobile = document.getElementById('nav-safety-button-mobile');
-const escapeButton = document.getElementById('nav-safety-button');
+const escapeButtonMobile = document.getElementById('safety-mobile');
+const escapeButton = document.getElementById('safety-notmobile');
 const buttons = [escapeButton, escapeButtonMobile];
 
     buttons.forEach((btn) => btn.addEventListener('click', (e) => {
